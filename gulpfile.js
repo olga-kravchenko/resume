d62a760f3f0bc3891ddb92e653d3666644c96fd5
Olga <olga19972010@yandex.by>
2021-07-17 16:47:25 +0300

    Connected the configuration files.

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+const gulpfile = require("gulp");
+const plumber = require("gulp-plumber");
+const sourcemap = require("gulp-sourcemaps");
+const sass = require("gulp-sass");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const sync = require("browser-sync").create();
+
+// Styles
+
+const styles = () => {
+    return gulpfile.src("source/sass/style.scss")
+        .pipe(plumber())
+        .pipe(sourcemap.init())
+        .pipe(sass())
+        .pipe(postcss([
+            autoprefixer()
+        ]))
+        .pipe(sourcemap.write("."))
+        .pipe(gulpfile.dest("source/css"))
+        .pipe(sync.stream());
+}
+
+exports.styles = styles;
+
+// Server
+
+const server = (done) => {
+    sync.init({
+        server: {
+            baseDir: 'source'
+        },
+        cors: true,
+        notify: false,
+        ui: false,
+    });
+    done();
+}
+
+exports.server = server;
+
+// Watcher
+
+const watcher = () => {
+    gulpfile.watch("source/sass/**/*.scss", gulpfile.series("styles"));
+    gulpfile.watch("source/*.html").on("change", sync.reload);
+}
+
+exports.default = gulpfile.series(
+    styles, server, watcher
+);
